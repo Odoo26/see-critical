3b7157a2ccc639763cb4c0d4189c94b193e1af2c
JULIA Gilles <g.julia@selecom.fr>
2022-10-28 14:18:47 +0200

    commint manifest

--- Site/__manifest__.py
+++ Site/__manifest__.py
@@ -7,7 +7,6 @@
     'depends': ['base'],
     'data': [
         'views/views.xml',
-        'views/vehicule.xml',
         'security/ir.model.access.csv',
         'views/sites_menu.xml',
         ],
