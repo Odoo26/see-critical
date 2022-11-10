65484742e5fc7edb2ceb300927723c5ea35a5c85
JULIA Gilles <g.julia@selecom.fr>
2022-11-10 10:23:53 +0100

    rajout contact

--- Site/models/sites.py
+++ Site/models/sites.py
@@ -1,7 +1,10 @@
 from odoo import models, fields
+
 class Sites(models.Model):
     _name = 'sites.sites'
+    _inherit = 'contacts'
 
+    contact = fields.Char(required=True,string="Contacts")
     name = fields.Char(string="Nom du site")
     types = fields.Many2one(comodel_name='types.sites',string='Type de materiel')
     adresse = fields.Char(string="Adresse")
