2fb221f14ebcb09136554c6c106727af39462ea5
JULIA Gilles <g.julia@selecom.fr>
2022-11-29 15:31:05 +0100

    crm apps

---
+++ crm_see/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name':'Solution CRM pour See',
+    'description':'Gérer les contacts commerciaux',
+    'author':'JULIA Gilles',
+    'license': "AGPL-3",
+    'depends':['crm'],
+    'data': [
+    'views/see.xml',
+    'data/data.xml',
+    ],
+    'application':False,
+}
